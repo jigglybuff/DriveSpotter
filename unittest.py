430ee20526d03126da42236388f5c8d4a70d3363
jigglybuff <njthomas@mtu.edu>
2017-04-20 13:47:19 -0400

    Update unittest.py

--- unittest.py
+++ unittest.py
@@ -3,12 +3,7 @@ from flask import request
 
 class MyTest(unittest.TestCase):
     def test_something(self):
-        with app.test_request_context('/?name=Peter'):
-            # Create a request context, which is needed to pass the URL parameter to a request object
-            # The with-statement automatically pushes on entry
-            # You can now view attributes on request context stack by using 'request'.
-            assert flask.request.path == '/'
-            assert flask.request.args['name'] == 'Peter'
-            # The with-statement automatically pops on exit
+        with app.test_request_context('/?name=Test'):
 
-        # After the with-statement, the request context stack is empty
+            assert flask.request.path == '/'
+            assert flask.request.args['name'] == 'Test'
